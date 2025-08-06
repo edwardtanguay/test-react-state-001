3691389531fba11e17ebb42fc57c3a87b8847537
Edward Tanguay (windows) <edward@tanguay.info>
2025-08-06 09:06:56 +0200

    example 2 layout

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 export const PageWelcome = () => {
+	const [isOnline, setIsOnline] = useState(false);
+
 	return (
-		<p>This is the welcome page.</p>
-	)
-}
+		<>
+			<div className="example">
+				<h2>Example 1</h2>
+				<div className="flex gap-1 items-center">
+					<button onClick={() => setIsOnline(!isOnline)}>
+						Change online status
+					</button>
+
+					<p className="text-xl">
+						User is {isOnline ? "online" : "offline"}.
+					</p>
+				</div>
+			</div>
+
+			<div className="example">
+				<h2>Example 2</h2>
+				<div className="flex gap-1 items-center">
+					<p className="text-xl">Password:</p>
+					<input type="password" className="border border-slate-300 rounded-md px-2 py-1" />
+					<button>Login</button>
+				</div>
+			</div>
+		</>
+	);
+};
