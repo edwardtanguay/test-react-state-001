3e0739eba6a4fc1e31cd89263fb0a750bf753e57
Edward Tanguay (windows) <edward@tanguay.info>
2025-08-06 09:49:24 +0200

    Finish ex 3

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -5,59 +5,74 @@ export const PageWelcome = () => {
 	const [thePassword, setThePassword] = useState("");
 	const [isFormDisabled, setIsFormDisabled] = useState(false);
 
-
 	const handleLoginButton = () => {
 		if (thePassword === "fantastic") {
 			setIsOnline(true);
 		}
-	}
+	};
 
 	const handleDisableForm = () => {
-		setIsFormDisabled(!isFormDisabled)
-	}
+		setIsFormDisabled(!isFormDisabled);
+	};
 
 	return (
 		<>
-			<div className="example">
-				<h2>Example 1</h2>
-				<div className="flex gap-1 items-center">
-					<button onClick={() => setIsOnline(!isOnline)}>
-						Change online status
-					</button>
-
-					<p className={`text-xl ${isOnline ? 'userIsOnline' : 'userIsOffline'}`}>
-						User is {isOnline ? "online" : "offline"}.
-					</p>
-				</div>
-			</div>
-
-			<div className="example">
-				<h2>Example 2</h2>
-				{isOnline ? (
-					<p className="text-xl">Enjoy the site.</p>
-				) : (
+			<div className={`${isFormDisabled ? 'formIsDisabled' : ''}`}>
+				<div className="example">
+					<h2>Example 1</h2>
 					<div className="flex gap-1 items-center">
-						<p className="text-xl">Password:</p>
-						<input
-							type="password"
-							value={thePassword}
-							onChange={(e) => setThePassword(e.target.value)}
-							className="border border-slate-300 rounded-md px-2 py-1"
-						/>
-						<button className={`${thePassword.length < 8 ? 'disabled' : ''}`} disabled={thePassword.length < 8} onClick={handleLoginButton}>Login</button>
+						<button onClick={() => setIsOnline(!isOnline)}>
+							Change online status
+						</button>
 
+						<p
+							className={`text-xl ${
+								isOnline ? "userIsOnline" : "userIsOffline"
+							}`}
+						>
+							User is {isOnline ? "online" : "offline"}.
+						</p>
 					</div>
-				)}
+				</div>
+
+				<div className="example">
+					<h2>Example 2</h2>
+					{isOnline ? (
+						<p className="text-xl">Enjoy the site.</p>
+					) : (
+						<div className="flex gap-1 items-center">
+							<p className="text-xl">Password:</p>
+							<input
+								type="password"
+								value={thePassword}
+								onChange={(e) => setThePassword(e.target.value)}
+								className="border border-slate-300 rounded-md px-2 py-1"
+							/>
+							<button
+								className={`${
+									thePassword.length < 8 ? "disabled" : ""
+								}`}
+								disabled={thePassword.length < 8}
+								onClick={handleLoginButton}
+							>
+								Login
+							</button>
+						</div>
+					)}
+				</div>
 			</div>
 
 			<div className="example">
 				<h2>Example 3</h2>
 				<div className="flex gap-1 items-center">
-					<input type="checkbox" id="disableForm" onClick={handleDisableForm}/>
+					<input
+						type="checkbox"
+						id="disableForm"
+						onClick={handleDisableForm}
+					/>
 					<label htmlFor="disableForm">Disable form</label>
 				</div>
 			</div>
-
 		</>
 	);
 };
